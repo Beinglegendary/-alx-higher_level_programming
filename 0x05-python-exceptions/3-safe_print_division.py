6db6af1a9e937ab226efb00a97d3c52cb3e46faf
Beinglegendary <99263767+Beinglegendary@users.noreply.github.com>
2022-06-20 15:34:41 +0100

    new commit

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,14 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
-        result = None
+
+    except ZeroDivisionError:
+        return (None)
+
     finally:
-        print("Inside result: {}".format(result))
-        return (result)
+        try:
+            print("Inside result: {:.1f}".format(result))
+            return (result)
+        except NameError:
+            print("Inside result: None")
+            return (None)
