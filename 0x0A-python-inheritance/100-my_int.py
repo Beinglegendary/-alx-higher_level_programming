78aeef8c4ec3f33d0fc2f5b6b145f189a7938c3f
UWABOR COLLINS <99263767+Beinglegendary@users.noreply.github.com>
2022-07-05 02:13:31 +0100

    NEW COMMIT

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""This program create a new int called MyInt and is rebel!!!"""
+
+
 class MyInt(int):
-    """ Class that inherits from class int"""
+    """Class MyInt is like an int but Rebel"""
 
-    def __eq__(self, other):
-        """ Method that returns != check """
-        return int.__ne__(self, other)
+    def __eq__(self, other_num):
+        """Rebel!!! equal is not equal"""
+        return super().__ne__(other_num)
 
-    def __ne__(self, other):
-        """ Method that returns == check """
-        return int.__eq__(self, other)
+    def __ne__(self, other_num):
+        """Rebel!!! not equal is equal"""
+        return super().__eq__(other_num)
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-def add_attribute(obj, name, value):
-    """ Function that adds a new attribute to an object
-    Args:
-        obj: object
-        name: attribute name
-        value: attribute value
-    Raises:
-        TypeError: when the attribute can't be added
+"""
+This program will try to add attributes to classes if it possible
+"""
+
+
+def add_attribute(obj, key, value):
+    """
+    This function will try to add a new attribute
+    to a class if it possible, throws an Exception if can't
     """
 
-    if not hasattr(obj, "__dict__"):
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
-    setattr(obj, name, value)
+
+    setattr(obj, key, value)
