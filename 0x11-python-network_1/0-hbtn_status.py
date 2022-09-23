24bb9978b24cd755f721c936c0a79bcb0d05db7d
UWABOR COLLINS <99263767+Beinglegendary@users.noreply.github.com>
2022-09-24 00:55:31 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,5 @@
 
+
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
@@ -12,6 +13,17 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
+
+
+
+
+        
+        
+
+
+
 
 
 
